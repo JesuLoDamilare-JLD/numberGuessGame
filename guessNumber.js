9e12633b0b33dcfb371f2d999757827bee4cf2dd
Dare Emmanuel <dareolumuyiwa@jldaveoplus.com>
2024-01-29 13:20:24 +0000

    update

--- guessNumber.js
+++ guessNumber.js
@@ -1,3 +1,14 @@
 "use strict";
+
+document.querySelector(".check-button").addEventListener("click", function () {
+  document.querySelector(".game-msg p").textContent = "🏆Correct Number!";
+  document.querySelector(".score span").textContent = 10;
+  document.querySelector(".input-value input").value = 23;
+});
+
 let randomNumber = Math.floor(Math.random() * 100) + 1;
 document.querySelector(".guess-number").innerHTML = randomNumber;
+
+let checkNumber = document.querySelector(".check-button");
+
+const inputNumber = () => {};
